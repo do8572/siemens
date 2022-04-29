ad6647ce85ce1e979d0917cc8dce582aa1a8ef13
kpirc <klementina.pirc@in516ht.com>
2022-04-29 17:59:40 +0200

    plot rearanged

--- src/Plots/data_analysis.py
+++ src/Plots/data_analysis.py
@@ -13,31 +13,27 @@ def unique_products_per_customer(data):
         product[cust] = customer_purchases['product'].nunique()
         family[cust] = customer_purchases['family'].nunique()
 
-    # Create 2x2 sub plots
-    gs = gridspec.GridSpec(2, 2, wspace=0.4, hspace=0.3)
+    gs = gridspec.GridSpec(1, 3, wspace=0.4)
 
-    pl.figure()
+    pl.figure(figsize=(8, 4))
     pl.tight_layout()
 
     ax = pl.subplot(gs[0, 0]) # row 0, col 0
     pl.hist(variation.values(),bins=list(range(0,420,20)), color='blue', label='unique variations')
     pl.xlabel('variations')
     pl.ylabel('customers')
-    ax.legend(bbox_to_anchor=(2.2, -0.45))
+    ax.legend(bbox_to_anchor=(1.05, 1.15))
 
     ax = pl.subplot(gs[0, 1]) # row 0, col 1
     pl.hist(product.values(),bins=list(range(0,210,10)), color='cornflowerblue', label='unique products')
     pl.xlabel('products')
-    pl.ylabel('customers')
-    ax.legend(bbox_to_anchor=(0.76, -0.72))
+    ax.legend(bbox_to_anchor=(1.05, 1.15))
 
-    ax = pl.subplot(gs[1, 0]) # row 1, span all columns
+    ax = pl.subplot(gs[0, 2]) # row 1, span all columns
     pl.hist(family.values(),bins=range(0,20,1), color='slateblue', label='unique family')
     pl.xlabel('families')
-    pl.ylabel('customers')
-    ax.legend(bbox_to_anchor=(2.08, 0.3))
+    ax.legend(bbox_to_anchor=(1, 1.15))
 
-    pl.suptitle('Product analysis per customer')
     plt.savefig('unique.png')
 
 if __name__ == '__main__':
