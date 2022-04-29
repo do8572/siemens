7b490e45291fc867d02669e17af00125bc9e1f72
kpirc <klementina.pirc@in516ht.com>
2022-04-29 12:24:42 +0200

    baseline for product and family prediction

--- src/baseline.py
+++ src/baseline.py
@@ -8,57 +8,67 @@ def most_common_category_per_customer(data):
     for cust in data['customer'].unique():
         customer_purchases = data[data['customer'] == cust]
         values = customer_purchases['family'].value_counts()
-        category = values.index[0]
+        category = values.index.tolist()
         categories[cust] = category
     return categories
 
-def most_popular_item_per_category(data):
-    popular_items = {}
-    for cat in data['family'].unique():
-        category_purchases = data[data['family'] == cat]
-        all_items = category_purchases['variation'].value_counts()
-        items = all_items.index.tolist()
-        popular_items[cat] = items
-    return popular_items
+def most_popular_variation_and_product_per_category(data):
+    popular_variations = {}
+    popular_products = {}
+    for fam in data['family'].unique():
+        category_purchases = data[data['family'] == fam]
 
-def baseline_with_most_popular_item(train, test):
-    popular_items = most_popular_item_per_category(train)
+        all_variations = category_purchases['variation'].value_counts()
+        variations = all_variations.index.tolist()
+        popular_variations[fam] = variations
+
+        all_products = category_purchases['product'].value_counts()
+        products = all_products.index.tolist()
+        popular_products[fam] = products
+    return popular_variations, popular_products
+
+def baseline_most_popular(train, test, prediction):
+    popular_variations, popular_products = most_popular_variation_and_product_per_category(train)
     categories = most_common_category_per_customer(train)
     
     predictions = 0
     correct_predictions = 0
-    customers_not_in_train_set = 0
     for cust in test['customer'].unique():
         try:
-            category = categories[cust]
-            items = popular_items[category]
-            purchased_items = test[test['customer'] == cust]['variation'].unique()
+            category = categories[cust][0]
+            if prediction == 'variation':
+                items = popular_variations[category]
+            elif prediction == 'product':
+                items = popular_products[category]
+            elif prediction == 'family':
+                items = categories[cust]
+            purchased_items = test[test['customer'] == cust][prediction].unique()
             for item in items[:5]:
                 if item in purchased_items:
                     correct_predictions += 1
                     break
             predictions += 1
         except:
-            customers_not_in_train_set += 1
             pass
-    #print('customers in test set: ', len(test['customer'].unique()))
-    #print('customers_not_in_train_set: ', customers_not_in_train_set)
     return correct_predictions / predictions
 
-def baseline_with_most_popular_new_item(train, test):
-    # top 5 variations include only those that a customer has not bought before
-    popular_items = most_popular_item_per_category(train)
+def baseline_most_popular_new(train, test, prediction):
+    popular_variations, popular_products = most_popular_variation_and_product_per_category(train)
     categories = most_common_category_per_customer(train)
     
     predictions = 0
     correct_predictions = 0
-    customers_not_in_train_set = 0
     for cust in test['customer'].unique():
         try:
-            category = categories[cust]
-            items = popular_items[category]
-            purchased_items_test = test[test['customer'] == cust]['variation'].unique()
-            purchased_items_train = train[train['customer'] == cust]['variation'].unique()
+            category = categories[cust][0]
+            if prediction == 'variation':
+                items = popular_variations[category]
+            elif prediction == 'product':
+                items = popular_products[category]
+            elif prediction == 'family':
+                items = categories[cust]
+            purchased_items_test = test[test['customer'] == cust][prediction].unique()
+            purchased_items_train = train[train['customer'] == cust][prediction].unique()
             j = 0
             for item in items:
                 if j == 5:
@@ -72,10 +82,7 @@ def baseline_with_most_popular_new_item(train, test):
                         break
             predictions += 1
         except:
-            customers_not_in_train_set += 1
             pass
-    #print('customers in test set: ', len(test['customer'].unique()))
-    #print('customers_not_in_train_set: ', customers_not_in_train_set)
     return correct_predictions / predictions
 
 def predict_random_item(items):
@@ -101,10 +108,10 @@ def one_split(data, split):
     train = data[data['day'] <= split]
     test = data[data['day'] > split]
 
-    ac = baseline_with_most_popular_item(train, test)
+    ac = baseline_most_popular(train, test, prediction='variation')
     print('popular variations', ac)
 
-    acc = baseline_with_most_popular_new_item(train, test)
+    acc = baseline_most_popular_new(train, test, prediction='variation')
     print('popular new variations', acc)
 
     accc = baseline_with_random_item(train, test)
@@ -113,9 +120,9 @@ def one_split(data, split):
 def sliding_window(data, train_size, test_size):
     print('SLIDING WINDOW')
     iterations = (1250 - train_size) // test_size
-    popular = []
-    popular_new = []
-    rand = []
+    
+    popular = {'variation':[], 'product':[], 'family':[]}
+    popular_new = {'variation':[], 'product':[], 'family':[]}
     for i in range(iterations):
         start = test_size * i
         split = test_size * i + train_size
@@ -124,23 +131,20 @@ def sliding_window(data, train_size, test_size):
         test = data[(data['day'] > split) & (data['day'] <= end)]
 
         print('window ', start, split, end)
+        for prediction in ['variation', 'product', 'family']:
+            ac = baseline_most_popular(train, test, prediction=prediction)
+            popular[prediction].append(ac)
+            print(f'popular {prediction}', ac)
 
-        ac = baseline_with_most_popular_item(train, test)
-        popular.append(ac)
-        print('popular variations', ac)
+            acc = baseline_most_popular_new(train, test, prediction=prediction)
+            popular_new[prediction].append(acc)
+            print(f'popular new {prediction}', acc)
 
-        acc = baseline_with_most_popular_new_item(train, test)
-        popular_new.append(acc)
-        print('popular new variations', acc)
-        
-        accc = baseline_with_random_item(train, test)
-        rand.append(accc)
-        print('random variations', accc)
-
+    print('--------------------------------------')
     print('mean values and standard deviation')
-    print('popular variations', np.mean(popular), np.std(popular))
-    print('popular new variations', np.mean(popular_new), np.std(popular_new))
-    print('random variations', np.mean(rand), np.std(rand)) 
+    for prediction in ['variation', 'product', 'family']:
+        print(f'popular {prediction}', np.mean(popular[prediction]), np.std(popular[prediction]))
+        print(f'popular new {prediction}', np.mean(popular_new[prediction]), np.std(popular_new[prediction]))
 
 if __name__ == '__main__':
     data = pd.read_csv('transaction_history.csv', sep=',', header=0)
@@ -148,7 +152,6 @@ if __name__ == '__main__':
     data['product'] = data['prod'].str[:9]
     data['variation'] = data['prod'].str[:14]
 
-    one_split(data, 1000)
+    #one_split(data, 1000)
     sliding_window(data, 500, 125)
     
-
